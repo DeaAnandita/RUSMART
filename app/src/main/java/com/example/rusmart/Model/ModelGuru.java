61032bb3be674f0c027b725e624f9ad402133bc3
Aji Suryawan <kazumovie132@gmail.com>
2019-10-06 00:01:28 +0700

    spinner guru sudah bisa online

---
+++ app/src/main/java/com/example/rusmart/Model/ModelGuru.java
@@ -0,0 +1,22 @@
+package com.example.rusmart.Model;
+
+public class ModelGuru {
+    private String kodeGuru;
+    private String namaGuru;
+
+    public String getKodeGuru() {
+        return kodeGuru;
+    }
+
+    public void setKodeGuru(String kodeGuru) {
+        this.kodeGuru = kodeGuru;
+    }
+
+    public String getNamaGuru() {
+        return namaGuru;
+    }
+
+    public void setNamaGuru(String namaGuru) {
+        this.namaGuru = namaGuru;
+    }
+}
